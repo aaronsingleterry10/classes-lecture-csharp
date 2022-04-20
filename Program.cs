17fe3ebfc285f881466308ad5069d4aed323c55a
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-19 22:57:13 -0500

    created customer class

--- Program.cs
+++ Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace ClassSection
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            var person = Person.Parse("James");
+            person.Introduce("Aaron");
+
         }
     }
 }
